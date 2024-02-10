89f94c5231ce3668790e9f7764d0bc1f45bf6273
unknown <jorge.tenorio@edu.uah.es>
2024-02-10 14:16:50 +0100

    Ejercicio 1.2- nueva clase Circle

---
+++ Circle.java
@@ -0,0 +1,26 @@
+public class Circle {
+    private double radius;
+
+    public Circle(){
+        this.radius=1.0;
+
+    }
+    public Circle(double r){
+        radius= r;
+    }
+    public double getRadius(){
+        return radius;
+    }
+    public void setRadius(double radius) {
+            this.radius = radius;
+    }
+    public double getArea(){
+        return Math.PI*(radius*radius);
+    }
+    public double getCircumference(){
+        return Math.PI*2*radius;
+    }
+    public String toString() {
+        return "Circle[radius=" + radius + " ]";
+    }
+}
--- Ejercicio1.java
+++
@@ -1,4 +0,0 @@
-public class Ejercicio1 {
-    ///Probando
-    ///Parra
-}
