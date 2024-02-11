c9212daea9664cdec35a93235a7beb150b395ef0
unknown <jorge.tenorio@edu.uah.es>
2024-02-11 13:37:50 +0100

    1.4 - Nueva clase employee

---
+++ es.uah.matcomp.mp.e1.ejerciciosclases/Rectangle.java
@@ -0,0 +1,37 @@
+public class Rectangle {
+    private float length;
+    private float width;
+
+    public Rectangle(float length, float width){
+        this.width=width;
+        this.length=length;
+
+    }
+    public Rectangle(){
+        this.length=1.0f;
+        this.width=1.0f;
+
+    }
+    public float getLength(){
+        return length;
+    }
+    public float getWidth(){
+        return width;
+    }
+    public void setLength(float length) {
+        this.length = length;
+    }
+    public void setWidth(float width){
+        this.width=width;
+    }
+    public double getArea(){
+        return length*width;
+    }
+    public double getPerimeter(){
+        return (2*length)+(2*width);
+    }
+    public String toString() {
+        return "Rectangle[length=" + length + " width= " + width + " ]";
+    }
+
+}
