ebe489e37317e2772fa97071c0f5a79dcfd63062
MarcosZamorano <157693210+MarcosZamorano@users.noreply.github.com>
2024-02-14 11:44:14 +0100

    1.6 Clase Account

--- Account.java
+++ Account.java
@@ -1,3 +1,48 @@
-package PACKAGE_NAME;public class Account {
-    //no funciona esto
+public class Account{
+    private String id;
+    private String name;
+    private int balance;
+    public Account(String id, String name){
+        this.id=id;
+        this.name=name;
+        this.balance=0;
+    }
+    public Account(String id, String name,int balance){
+        this.id=id;
+        this.name=name;
+        this.balance=balance;
+    }
+
+    public String getId(){
+        return id;
+    }
+    public String getName(){
+        return name;
+    }
+    public int getBalance(){
+        return balance;
+    }
+    public int credit(int amount) {
+        this.balance = balance + amount;
+        return balance;
+    }
+    public int debit(int amount) {
+        if (amount <= balance){
+            this.balance = balance - amount;}
+        else{
+            System.out.println("Amount exceeded balance");}
+        return balance;
+    }
+    public int transferTo(Account anotherAccount,int amount){
+        if (amount <= balance) {
+            this.balance -= amount;
+            anotherAccount.credit(amount);
+        }
+        else
+            System.out.println("Amount exceeded balance");
+        return balance;
+    }
+    public String toString() {
+        return "Account[id=" + id + " name= " + name + " balance= "+ balance+ "]";
+    }
 }
