0bf4406c1b20840011a8e563722385f2925597f9
unknown <jorge.tenorio@edu.uah.es>
2024-02-24 19:41:25 +0100

    3.4 Ejercicio: Point y MovablePoint

--- MovablePoint.java
+++ MovablePoint.java
@@ -1,2 +1,54 @@
-package PACKAGE_NAME;public class MovablePoint {
+public class MovablePoint extends Point{
+    private float xSpeed;
+    private float ySpeed;
+    public MovablePoint(float x, float y, float xSpeed,float ySpeed){
+        super(x,y);
+        this.xSpeed=xSpeed;
+        this.ySpeed=ySpeed;
+    }
+    public MovablePoint(float xSpeed, float ySpeed){
+        this.xSpeed=xSpeed;
+        this.ySpeed=ySpeed;
+    }
+    public MovablePoint(){
+        xSpeed=0.0f;
+        ySpeed=0.0f;
+    }
+
+    public float getxSpeed() {
+        return xSpeed;
+    }
+
+    public float getySpeed() {
+        return ySpeed;
+    }
+
+    public void setxSpeed(float xSpeed) {
+        this.xSpeed = xSpeed;
+    }
+
+    public void setySpeed(float ySpeed) {
+        this.ySpeed = ySpeed;
+    }
+    public void setSpeed(float xSpeed, float ySpeed){
+        this.xSpeed=xSpeed;
+        this.ySpeed=ySpeed;
+    }
+    public float[] getSpeed(){
+        float[] speed= new float[2];
+        speed[0]=xSpeed;
+        speed[1]=ySpeed;
+        return speed;
+    }
+
+    @Override
+    public String toString() {
+        return "("+getX()+","+getY()+")"+",speed=("+ xSpeed+","+ySpeed+")";
+    }
+    MovablePoint move(){
+        x+=xSpeed;
+        y+=ySpeed;
+        return this;
+    }
 }
+//SubClase MovablePoint
--- Point.java
+++ Point.java
@@ -1,2 +1,45 @@
 public class Point {
+    private float x;
+    private float y;
+
+    public Point(float x, float y){
+        this.x=x;
+        this.y=y;
+    }
+    public Point(){
+        x=0.0f;
+        y=0.0f;
+    }
+
+    public float getX() {
+        return x;
+    }
+
+    public float getY() {
+        return y;
+    }
+
+    public void setY(float y) {
+        this.y = y;
+    }
+
+    public void setX(float x) {
+        this.x = x;
+    }
+    public void setXY(float x, float y){
+        this.x=x;
+        this.y=y;
+    }
+    public float[] getXY(){
+        float[] xy= new float[2];
+        xy[0]=x;
+        xy[1]=y;
+        return xy;
+    }
+
+    @Override
+    public String toString() {
+        return "("+x+","+y+")";
+    }
 }
+//SuperClase Point
