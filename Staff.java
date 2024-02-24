b25ffb8781d4c50acb6e007b43a7479a33232dbe
unknown <jorge.tenorio@edu.uah.es>
2024-02-24 19:13:02 +0100

    3.2 Ejercicio: superclase Person y sus subclases

--- Staff.java
+++ Staff.java
@@ -1,2 +1,35 @@
-package PACKAGE_NAME;public class Staff {
+public class Staff extends Person {
+    private String school;
+    private double pay;
+    public Staff(String name, String address, String school, double pay){
+        super(name,address);
+        this.school=school;
+        this.pay=pay;
+    }
+
+    public String getSchool() {
+        return school;
+    }
+
+    public double getPay() {
+        return pay;
+    }
+
+    public void setPay(double pay) {
+        this.pay = pay;
+    }
+
+    public void setSchool(String school) {
+        this.school = school;
+    }
+
+    @Override
+    public String toString() {
+        return "Staff{" +
+                "[name=" + getName() + ", address=" + getAddress() +
+                "school='" + school + '\'' +
+                ", pay=" + pay +
+                ']';
+    }
 }
+//Subclase Staff
