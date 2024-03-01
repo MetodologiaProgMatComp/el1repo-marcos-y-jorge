7bc5db40e9a19852544a3547a9f25a6c89a2ae94
unknown <jorge.tenorio@edu.uah.es>
2024-03-01 13:16:42 +0100

    Test y correción de errores

--- EjerciciosPolimorfismo/TestResizableCircle.java
+++ EjerciciosPolimorfismo/TestResizableCircle.java
@@ -1,2 +1,36 @@
-package EjerciciosPolimorfismo;public class TestResizableCircle {
+package EjerciciosPolimorfismo;
+
+public class TestResizableCircle {
+        public static void main(String[] args) {
+            // Crear un círculo redimensionable con radio 5
+            ResizableCircle resizableCircle = new ResizableCircle(5);
+
+            // Imprimir detalles del círculo redimensionable antes de la modificación
+            System.out.println("Resizable Circle before resizing:");
+            System.out.println("Radius: " + resizableCircle.radius);
+            System.out.println("Perimeter: " + resizableCircle.getPerimeter());
+            System.out.println("Area: " + resizableCircle.getArea());
+            System.out.println("String representation: " + resizableCircle.toString());
+
+            // Redimensionar el círculo redimensionable al 50%
+            resizableCircle.resize(50);
+
+            // Imprimir detalles del círculo redimensionable después de la modificación
+            System.out.println("\nResizable Circle after resizing:");
+            System.out.println("Radius: " + resizableCircle.radius);
+            System.out.println("Perimeter: " + resizableCircle.getPerimeter());
+            System.out.println("Area: " + resizableCircle.getArea());
+            System.out.println("String representation: " + resizableCircle.toString());
+
+            // Crear un círculo regular con radio 3
+            Circle3 circle = new Circle3(3);
+
+            // Imprimir detalles del círculo regular
+            System.out.println("\nCircle details:");
+            System.out.println("Radius: " + circle.radius);
+            System.out.println("Perimeter: " + circle.getPerimeter());
+            System.out.println("Area: " + circle.getArea());
+            System.out.println("String representation: " + circle.toString());
+        }
+
 }
--- EjerciciosPolimorfismo/TestShape.java
+++ EjerciciosPolimorfismo/TestShape.java
@@ -1,2 +1,26 @@
-package EjerciciosPolimorfismo;public class TestShape {
-}
+package EjerciciosPolimorfismo;
+
+public class TestShape {
+        public static void main(String[] args) {
+            // Crear instancias de Shape
+            Shape shape1 = new Circle(5.0, "blue", true);
+            Shape shape2 = new Rectangle(4.0, 6.0, "green", false);
+
+            // Imprimir detalles de las formas
+            System.out.println("Shape 1:");
+            System.out.println("Color: " + shape1.getColor());
+            System.out.println("Filled: " + shape1.getFilled());
+            System.out.println("Area: " + shape1.getArea());
+            System.out.println("Perimeter: " + shape1.getPerimeter());
+            System.out.println(shape1);
+            System.out.println();
+
+            System.out.println("Shape 2:");
+            System.out.println("Color: " + shape2.getColor());
+            System.out.println("Filled: " + shape2.getFilled());
+            System.out.println("Area: " + shape2.getArea());
+            System.out.println("Perimetro: " + shape2.getPerimeter());
+            System.out.println(  shape2);
+        }
+    }
+
